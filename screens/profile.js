12ce5ff112c7c8b4cc3d182cc72246ce2d1d4d77
cduffy31 <callanduffy07@gmail.com>
2022-07-30 00:32:03 +0100

    icon

--- screens/profile.js
+++ screens/profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Image, View, Text, StyleSheet} from 'react-native';
+import {Image, View, Text, StyleSheet, Button} from 'react-native';
 import {Auth} from 'aws-amplify';
+import Icon from '../assets/icons/Icon';
 
 import {
     StyledContainer,
@@ -26,9 +27,18 @@ const {brand, darkLight} = Colors;
 const Profile = ({navigation}) => {
     return (
         <View style={styles.page}>
-
+            <View>
+                <View >
+                    <View style={styles.button}>
+                        <Icon name="" size={80} color="#000000"/>
+                        <Text>
+                            Account Settings
+                        </Text>
+                    </View>
+                </View>
+            </View>
             <StyledButton style={styles.logout}>
-                <ButtonText style={{}}>
+                <ButtonText>
                     Log Out
                 </ButtonText>
             </StyledButton>
@@ -42,9 +52,31 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'center',
     },
+    button:{
+        backgroundColor:'#ffffff',
+        border:1,
+        padding:10,
+        marginBottom:15,
+        width:'90%',
+        shadowColor: '#7F5DF0',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        elevation: 5,
+        padding:5, 
+        borderRadius: 10,
+
+    },
     page:{
-        justifyContent:'center',
         alignItems:'center',
+        flex:1,
+        justifyContent:'space-between',
+        backgroundColor:'#F5F5F5',
+        flexGrow: 1,
+        alignItems:'center'
     }
 })
 export default Profile;
